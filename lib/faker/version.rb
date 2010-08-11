535f0131599becec07883c1c1025cf8bd14dbbad
Jordan Brock <jordanbrock@gmail.com>
2010-08-11 16:48:23 +0800

    version stuff

--- lib/faker/version.rb
+++ lib/faker/version.rb
@@ -1,9 +1,3 @@
 module Faker #:nodoc:
-  module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 3
-    TINY  = 2
-
-    STRING = [MAJOR, MINOR, TINY].join('.')
-  end
+  VERSION = "0.3.2" unless defined?(::Faker::VERSION)
 end
